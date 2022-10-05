5029356610579d0a0ec2224b42307dbb78839d9d
Roman Lysiuk <gekalo1025@gmail.com>
2022-10-05 14:43:01 +0300

    feat: add js amount panel in pick friend block

--- online-zoo/src/pages/donate/donate.js
+++ online-zoo/src/pages/donate/donate.js
@@ -1,3 +1,43 @@
 import "../../stylesheets/pages/_donate.scss";
 import "../../stylesheets/main.scss";
 import "../home/burger";
+import { check } from "prettier";
+
+let form = document.forms.donationsAmount;
+let amountInput = document.querySelector(".another-amount__input");
+
+form.addEventListener("change", () => {
+  for (let index = 0; index < form.length; index++) {
+    if (form[index].checked) {
+      amountInput.value = form[index].value;
+    }
+  }
+});
+amountInput.addEventListener("input", () => {
+  switch (amountInput.value) {
+    case "5000":
+      document.getElementById("donations-amount-5000").checked = true;
+      break;
+    case "2000":
+      document.getElementById("donations-amount-2000").checked = true;
+      break;
+    case "1000":
+      document.getElementById("donations-amount-1000").checked = true;
+      break;
+    case "500":
+      document.getElementById("donations-amount-500").checked = true;
+      break;
+    case "100":
+      document.getElementById("donations-amount-100").checked = true;
+      break;
+    case "50":
+      document.getElementById("donations-amount-50").checked = true;
+      break;
+    case "25":
+      document.getElementById("donations-amount-25").checked = true;
+      break;
+
+    default:
+      break;
+  }
+});
